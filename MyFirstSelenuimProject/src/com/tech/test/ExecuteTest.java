19ca76b5ce8c4ea6f9c911bcda3155d483282ef4
sharandsouza <sharandsouza87@gmail.com>
2019-01-28 12:31:15 +0530

    cccc

---
+++ MyFirstSelenuimProject/src/com/tech/test/ExecuteTest.java
@@ -0,0 +1,18 @@
+package com.tech.test;
+
+import java.util.Properties;
+
+import com.tech.operations.readobjects;
+
+public class ExecuteTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		readobjects r = new readobjects();
+		Properties p = r.getMETHOD
+		p.getProperty("url");
+
+	}
+
+}
